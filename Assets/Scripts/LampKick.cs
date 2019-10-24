082c33e01786ae9d16a9b50eda880bd0f6265746
Ben Gilbert <bengilbert132@gmail.com>
2019-10-24 15:04:13 -0700

    Worked in the PhysicsTest scene. Created a duplicate lantern prefab for testing. Made a LanternSpawner script and LanternKick script for launching the lanterns.

---
+++ Assets/Scripts/LampKick.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class LampKick : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private float kickForce = 100f;
+    private bool pushing = false;
+    Vector3 pushForce;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        pushForce = new Vector3(UnityEngine.Random.Range(-0.5f, 0.5f), UnityEngine.Random.Range(0f, 0.5f), UnityEngine.Random.Range(0f, 0.5f));
+    }
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(UnityEngine.Random.Range(-50f, 50f), UnityEngine.Random.Range(0f, 50f), kickForce);
+        Debug.Log("Kick the fucker");
+        pushing = true;
+        StartCoroutine(PushTimer());
+        Invoke("Stop", 5);
+    }
+
+    private IEnumerator PushTimer()
+    {
+        
+        while (pushing)
+        {
+            Debug.Log(pushing);
+            rb.AddForce(pushForce);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    private void Stop()
+    {
+        pushing = false;
+    }
+}
---
+++ Assets/Scripts/LampSpawn.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampSpawn : MonoBehaviour
+{
+
+    public GameObject lampSpawner;
+    public GameObject lamp;
+    private bool cooldown = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cooldown = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (cooldown == false)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(lamp, lampSpawner.transform.position, lampSpawner.transform.rotation);
+                Debug.Log("Lantern spawned");
+                cooldown = true;
+                StartCoroutine(CoolTimer());
+            }
+        }
+        else
+            Debug.Log("Fuck you");
+    }
+
+    private IEnumerator CoolTimer()
+    {
+        yield return new WaitForSeconds(2);
+        cooldown = false;
+    }
+}
